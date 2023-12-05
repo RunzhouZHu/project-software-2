d7f96c4b77681ce110f469c58616f54fc62c16cd
Runzhou Zhu <runzhouz@metropolia.fi>
2023-12-05 15:03:55 +0200

    123

--- JavaScript/Shop.js
+++ JavaScript/Shop.js
@@ -1,3 +1,18 @@
+const shop_list = [{
+    'airplane_id':1,
+    'airplane_type_name':'small',
+    'fuel_volume': 10000,
+    'fuel_per_kilo':10
+},
+    {
+        'airplane_id':2,
+        'airplane_type_name':'medium',
+        'fuel_volume': 100000,
+        'fuel_per_kilo':10
+    },
+]
+
+
 // Shop Page
 const shop = document.getElementById('shop')
 shop.addEventListener('click', function (evt) {
@@ -7,6 +22,16 @@ shop.addEventListener('click', function (evt) {
 
 // Shop Icon
 const shop_icon = document.getElementById('shop_icon')
+const goods = document.createElement('p')
 shop_icon.addEventListener('click', function (evt) {
+
     shop.style.display = 'block';
+
+    for (let i=0;i<shop_list.length;i++)
+    {
+        goods.innerText =shop_list[i].airplane_type_name
+
+        shop.appendChild(goods)
+    }
+
 })
--- JavaScript/Tasks.js
+++ JavaScript/Tasks.js
@@ -1,14 +1,14 @@
 // Task example, should fetch from database
-const tasks_info = [{
+const tasks_list = [{
     'task_id': 1,
-    'task_name': 'example_task_name',
-    'task_text': 'example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text ',
+    'task_name': 'example_task_name1',
+    'task_text': '<p>text example task text example task text example task text example task text example task1111111</p>',
     'task_others': 'jjsjsjsjjs'
 },
     {
         'task_id': 2,
-        'task_name': 'example_task_name',
-        'task_text': 'example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text example task text ',
+        'task_name': 'example_task_name2',
+        'task_text': '<p>example task text example task text example task text example task text example task22222222</p>',
         'task_others': 'jjsjsjsjjs'
     },
 ]
@@ -16,16 +16,28 @@ const tasks_info = [{
 // Tasks
 const task_info = document.getElementById('task_info')
 const tasks = document.getElementById('tasks')
-tasks.innerHTML = "<img src=\"Css/pics/tasks.png\" alt=\"tasks\">\n" +
-    "<article> task task task111111 </article>"
 
-tasks.addEventListener('click', function (evt) {
-    task_info.style.display = 'block';
-})
+for (let i=0;i<tasks_list.length;i++)
+{
+    const task_name = document.createElement('p')
+    task_name.innerText = tasks_list[i].task_name
+    tasks.appendChild(task_name)
+
+    task_name.addEventListener('click', function (evt) {
+
+        task_info.innerHTML ="<img src='Css/pics/task_info1.png' alt='task_info'>"+
+            "<article>"+ tasks_list[i].task_text
+            +"<img src='Css/pics/task_pic.png' alt='task_pic'/>"
+            +"</article>"
+
+        task_info.style.display = 'block';
+    })
+}
 
 // Task info
 task_info.addEventListener('click', function (evt) {
     task_info.style.display = 'none';
+    task_info.innerHTML = '';
 })
 
 
