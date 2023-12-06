d2186897ae44ddd71f4102998525cb7bfe519bd5
Runzhou Zhu <runzhouz@metropolia.fi>
2023-12-06 17:04:26 +0200

    123

--- JavaScript/Tasks.js
+++ JavaScript/Tasks.js
@@ -1,15 +1,15 @@
 // Task example, should fetch from database
 const tasks_list = [{
     'task_id': 1,
-    'task_name': 'example_task_name1',
+    'task_name': 'My first airplane',
     'task_text': '<p>text example task text example task text example task text example task text example task1111111</p>',
-    'task_others': 'jjsjsjsjjs'
+    'task_pic': 'Css/pics/task_pics/task_pic1.jpg'
 },
     {
         'task_id': 2,
         'task_name': 'example_task_name2',
         'task_text': '<p>example task text example task text example task text example task text example task22222222</p>',
-        'task_others': 'jjsjsjsjjs'
+        'task_pic': 'Css/pics/task_pics/task_pic2.jpg'
     },
 ]
 
@@ -17,13 +17,13 @@ const version_tasks_list = [{
     'task_id': 1,
     'task_name': 'example_version_task_name1',
     'task_text': '<p>text example task text example task text example task text example task text example task1111111</p>',
-    'task_others': 'jjsjsjsjjs'
+    'task_pic': 'Css/pics/task_pics/version_task_pic1.jpg'
 },
     {
         'task_id': 2,
         'task_name': 'example_version_task_name2',
         'task_text': '<p>example task text example task text example task text example task text example task22222222</p>',
-        'task_others': 'jjsjsjsjjs'
+        'task_pic': 'Css/pics/task_pics/version_task_pic1.jpg'
     },
 ]
 
@@ -43,8 +43,9 @@ for (let i=0;i<tasks_list.length;i++)
     task_name.addEventListener('click', function (evt) {
 
         task_info.innerHTML ="<img src='Css/pics/task_info1.png' alt='task_info'>"+
-            "<article>"+ tasks_list[i].task_text
-            +"<img src='Css/pics/task_pic.png' alt='task_pic'/>"
+            "<article>"
+            +"<img src=" + tasks_list[i].task_pic + " alt='task_pic'/>"
+            + tasks_list[i].task_text
             +"</article>"
 
         task_info.style.display = 'block';
@@ -64,8 +65,9 @@ const version_tasks_icon = document.getElementById('version_tasks_icon')
 version_tasks_icon.addEventListener('click', function (evt) {
 
     version_tasks.innerHTML = "<img src='Css/pics/version_task.gif' alt='version task'>'"+
-            "<article>"+ version_tasks_list[1].task_text
-            +"<img src='Css/pics/task_pic.png' alt='task_pic'>"
+            "<article>"
+            +"<img src=" + version_tasks_list[1].task_pic + " alt='task_pic'/>"
+            + version_tasks_list[1].task_text
             +"</article>"
 
     version_tasks.style.display = 'block';
