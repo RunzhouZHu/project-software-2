aa282098e4ab6a6eef60ad2fc5b61feb9124fa8a
zhiyongzhou <Zhiyong.Zhou@metropolia.fi>
2023-12-06 19:58:44 +0200

    20231206

--- sql/airplane.sql
+++ sql/airplane.sql
@@ -11,7 +11,7 @@
  Target Server Version : 110102
  File Encoding         : 65001
 
- Date: 06/12/2023 01:51:50
+ Date: 06/12/2023 19:55:06
 */
 
 SET NAMES utf8mb4;
@@ -27,13 +27,14 @@ CREATE TABLE `airplane`  (
   `fuel_volume` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `fuel_per_kilo` int(20) NULL DEFAULT NULL,
   `airplane_pic` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `airplane_price` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`airplane_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of airplane
 -- ----------------------------
-INSERT INTO `airplane` VALUES (1, 'new_plane', '100000', 10, NULL);
-INSERT INTO `airplane` VALUES (5, 'sec_plane', '111111', 10, NULL);
+INSERT INTO `airplane` VALUES (1, 'new_plane', '100000', 10, NULL, NULL);
+INSERT INTO `airplane` VALUES (5, 'sec_plane', '111111', 10, NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
